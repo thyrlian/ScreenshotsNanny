56dd469ac7e8abb7b7325f0e3c7fc6f9133429a2
Jing Li <jingli@ebay.com>
2015-08-26 22:07:04 +0200

    Remove menu from demo activity

--- app/src/main/java/com/basgeekball/screenshotsnanny/DemoActivity.java
+++ app/src/main/java/com/basgeekball/screenshotsnanny/DemoActivity.java
@@ -1,9 +1,7 @@
 package com.basgeekball.screenshotsnanny;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -11,27 +9,5 @@ public class DemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_demo, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
