98e6a89d30ef9c2de5830e2a5dd5ddd028407f17
Jing Li <jingli@ebay.com>
2015-08-26 22:59:00 +0200

    Add library module

---
+++ library/src/androidTest/java/com/basgeekball/screenshotsnanny/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.basgeekball.screenshotsnanny;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
