c512cc45084efb4b12f44b0d72e7bebeeaff107e
Jing Li <jingli@ebay.com>
2015-08-27 23:55:01 +0200

    Rename DemoActivity to MainActivity

--- demo/src/main/java/com/basgeekball/screenshotsnanny/demo/MainActivity.java
+++ demo/src/main/java/com/basgeekball/screenshotsnanny/demo/MainActivity.java
@@ -3,11 +3,11 @@ package com.basgeekball.screenshotsnanny.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class DemoActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo);
+        setContentView(R.layout.activity_main);
     }
 }
