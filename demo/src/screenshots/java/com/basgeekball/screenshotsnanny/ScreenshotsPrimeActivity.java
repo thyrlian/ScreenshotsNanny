2424d4a6149d01ea830f02cbce269ee733b4bc74
Jing Li <jingli@ebay.com>
2015-09-01 21:21:13 +0200

    Add product flavors and new launcher activity for taking screenshots

---
+++ demo/src/screenshots/java/com/basgeekball/screenshotsnanny/ScreenshotsPrimeActivity.java
@@ -0,0 +1,15 @@
+package com.basgeekball.screenshotsnanny;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.basgeekball.screenshotsnanny.demo.R;
+
+public class ScreenshotsPrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screenshots_prime);
+    }
+}
