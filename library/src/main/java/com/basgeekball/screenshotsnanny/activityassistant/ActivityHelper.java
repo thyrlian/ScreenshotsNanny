0f30a6dbf36d0aa5b99a9821d7dff4c6854939e0
Jing Li <jingli@ebay.com>
2015-10-21 19:46:35 +0200

    Add activity readiness check method

--- library/src/main/java/com/basgeekball/screenshotsnanny/activityassistant/ActivityHelper.java
+++ library/src/main/java/com/basgeekball/screenshotsnanny/activityassistant/ActivityHelper.java
@@ -2,7 +2,12 @@ package com.basgeekball.screenshotsnanny.activityassistant;
 
 import android.app.Activity;
 import android.os.Build;
+import android.os.Handler;
 import android.util.ArrayMap;
+import android.util.Log;
+
+import com.basgeekball.screenshotsnanny.core.Constants;
+import com.basgeekball.screenshotsnanny.helper.Callback;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -15,6 +20,10 @@ public class ActivityHelper {
     private static final Field mPausedField;
     private static final Field mStoppedField;
     private static final Field mActivityField;
+
+    private static final long mWait = 100;
+    private static final int mRetriesMax = 20;
+    private static int mRetriesCounter = 0;
 
     // initialization
     static {
@@ -72,5 +81,26 @@ public class ActivityHelper {
             e.printStackTrace();
         }
         return activity;
+    }
+
+    public static void doSomethingWhenActivityIsReady(final Class<?> T, final Callback callback) {
+        if (mRetriesCounter < mRetriesMax) {
+            if (T.isInstance(getCurrentActivity())) {
+                Log.i(Constants.LOG_TAG, "⚒ Activity is ready, just do it");
+                mRetriesCounter = 0;
+                callback.execute();
+            } else {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        doSomethingWhenActivityIsReady(T, callback);
+                    }
+                }, mWait);
+                mRetriesCounter += 1;
+            }
+        } else {
+            Log.i(Constants.LOG_TAG, String.format("☠ Timeout: can not find %s", T.getSimpleName()));
+            mRetriesCounter = 0;
+        }
     }
 }
