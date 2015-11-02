eddd30836d6d6d03050433102089fc4421a071c1
Jing Li <jingli@ebay.com>
2015-11-02 06:20:06 +0100

    Refactor duplicated code in ScreenshotsTask

--- library/src/main/java/com/basgeekball/screenshotsnanny/core/ScreenshotsTask.java
+++ library/src/main/java/com/basgeekball/screenshotsnanny/core/ScreenshotsTask.java
@@ -7,7 +7,7 @@ import com.basgeekball.screenshotsnanny.helper.Callback;
 import com.basgeekball.screenshotsnanny.helper.KeyboardHelper;
 
 public class ScreenshotsTask {
-    public static void perform(final Class<?> T, final Callback callback, final Callback completionCallback, final long screenshotDelay) {
+    private static void perform(final Class<?> T, final Callback callback, final Callback completionCallback, final boolean hasMap, final long screenshotDelay, final int mapFragmentId) {
         new Runnable() {
             @Override
             public void run() {
@@ -16,31 +16,27 @@ public class ScreenshotsTask {
                     @Override
                     public void execute() {
                         KeyboardHelper.hideKeyboard();
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                ScreenshotsCapturer.execute(ActivityHelper.getCurrentActivity(), completionCallback);
-                            }
-                        }, screenshotDelay);
+                        if (hasMap) {
+                            ScreenshotsCapturer.executeWithMap(ActivityHelper.getCurrentActivity(), mapFragmentId, completionCallback);
+                        } else {
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ScreenshotsCapturer.execute(ActivityHelper.getCurrentActivity(), completionCallback);
+                                }
+                            }, screenshotDelay);
+                        }
                     }
                 });
             }
         }.run();
     }
 
-    public static void perform(final Class<?> T, final Callback callback, final Callback completionCallback, final int mapFragmentId) {
-        new Runnable() {
-            @Override
-            public void run() {
-                callback.execute();
-                ActivityHelper.performTaskWhenActivityIsReady(T, new Callback() {
-                    @Override
-                    public void execute() {
-                        KeyboardHelper.hideKeyboard();
-                        ScreenshotsCapturer.executeWithMap(ActivityHelper.getCurrentActivity(), mapFragmentId, completionCallback);
-                    }
-                });
-            }
-        }.run();
+    public static void perform(Class<?> T, Callback callback, Callback completionCallback, long screenshotDelay) {
+        perform(T, callback, completionCallback, false, screenshotDelay, 0);
+    }
+
+    public static void perform(Class<?> T, Callback callback, Callback completionCallback, int mapFragmentId) {
+        perform(T, callback, completionCallback, true, 0, mapFragmentId);
     }
 }
