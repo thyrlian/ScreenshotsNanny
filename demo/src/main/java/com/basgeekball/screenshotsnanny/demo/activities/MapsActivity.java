67a06f931bb87f946b9257fae131fd1054b8b52c
Jing Li <jingli@ebay.com>
2015-11-04 21:17:22 +0100

    Make TextInputLayout in maps activity not editable

--- demo/src/main/java/com/basgeekball/screenshotsnanny/demo/activities/MapsActivity.java
+++ demo/src/main/java/com/basgeekball/screenshotsnanny/demo/activities/MapsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.EditText;
 
 import com.basgeekball.screenshotsnanny.demo.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,6 +12,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -18,6 +20,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private final double mBerlinLat = 52.52;
     private final double mBerlinLng = 13.40;
     private final float mBerlinZoomLevel = 10;
+    private EditText mEditTextLat;
+    private EditText mEditTextLng;
 
     public static Intent createIntent(Activity activity) {
         return new Intent(activity, MapsActivity.class);
@@ -29,13 +33,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mEditTextLat = (EditText) findViewById(R.id.lat);
+        mEditTextLng = (EditText) findViewById(R.id.lng);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         LatLng berlin = new LatLng(mBerlinLat, mBerlinLng);
-        googleMap.addMarker(new MarkerOptions().position(berlin).title(getString(R.string.map_marker_title)));
+        Marker marker = googleMap.addMarker(new MarkerOptions().position(berlin).title(getString(R.string.map_marker_title)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(berlin));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(mBerlinZoomLevel));
+        mEditTextLat.setText(String.valueOf(marker.getPosition().latitude));
+        mEditTextLng.setText(String.valueOf(marker.getPosition().longitude));
     }
 }
