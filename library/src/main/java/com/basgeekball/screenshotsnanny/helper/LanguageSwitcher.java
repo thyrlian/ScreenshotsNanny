7217656267c08d009f75e2a18d8e6ce3fa9ef28b
Jing Li <jingli@ebay.com>
2015-11-21 10:06:54 +0100

    Add log to LanguageSwitcher

--- library/src/main/java/com/basgeekball/screenshotsnanny/helper/LanguageSwitcher.java
+++ library/src/main/java/com/basgeekball/screenshotsnanny/helper/LanguageSwitcher.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
+import android.util.Log;
 
 import java.util.Locale;
+
+import static com.basgeekball.screenshotsnanny.core.Constants.LOG_TAG;
 
 public class LanguageSwitcher {
 
@@ -19,6 +22,7 @@ public class LanguageSwitcher {
             Configuration configuration = resources.getConfiguration();
             configuration.locale = new Locale(language.toLowerCase());
             resources.updateConfiguration(configuration, displayMetrics);
+            Log.i(LOG_TAG, "⚙ Language is set to: " + language.toUpperCase());
         }
     }
 
